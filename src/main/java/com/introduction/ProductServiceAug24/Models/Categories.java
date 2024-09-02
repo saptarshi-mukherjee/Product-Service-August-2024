f186d7086ff7b1885a4ff9ec38bd945d45798563
saptarshi <sapbum1234@gmail.com>
2024-09-03 00:25:11 +0530

    Multiple Get calls

---
+++ src/main/java/com/introduction/ProductServiceAug24/Models/Categories.java
@@ -0,0 +1,37 @@
+package com.introduction.ProductServiceAug24.Models;
+
+public class Categories {
+    String cat1, cat2, cat3, cat4;
+
+    public String getCat1() {
+        return cat1;
+    }
+
+    public void setCat1(String cat1) {
+        this.cat1 = cat1;
+    }
+
+    public String getCat2() {
+        return cat2;
+    }
+
+    public void setCat2(String cat2) {
+        this.cat2 = cat2;
+    }
+
+    public String getCat3() {
+        return cat3;
+    }
+
+    public void setCat3(String cat3) {
+        this.cat3 = cat3;
+    }
+
+    public String getCat4() {
+        return cat4;
+    }
+
+    public void setCat4(String cat4) {
+        this.cat4 = cat4;
+    }
+}
