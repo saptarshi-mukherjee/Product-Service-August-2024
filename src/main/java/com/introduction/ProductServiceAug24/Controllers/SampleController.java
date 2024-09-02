f0cfeee4113524b8cdf3e1a3affb9f0060135569
saptarshi <sapbum1234@gmail.com>
2024-09-02 19:21:30 +0530

    Sample Hello World Controller

---
+++ src/main/java/com/introduction/ProductServiceAug24/Controllers/SampleController.java
@@ -0,0 +1,26 @@
+package com.introduction.ProductServiceAug24.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequestMapping("/hello")
+public class SampleController {
+
+    @GetMapping("/hello")
+    public String printString() {
+        return "Hello World!! First Project";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String printName(@PathVariable("name") String name) {
+        return "Hello "+name+". Welcome to SpringBoot Project";
+    }
+
+    @GetMapping("/shows/{show_name}/seats/{seat_id}")
+    public String showSeatStatus(@PathVariable("show_name") String show, @PathVariable("seat_id") int seat_num) {
+        return "Seat for "+show+" having seat number "+seat_num;
+    }
+}
---
+++ src/test/java/com/introduction/ProductServiceAug24/ProductServiceAug24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.introduction.ProductServiceAug24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceAug24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
