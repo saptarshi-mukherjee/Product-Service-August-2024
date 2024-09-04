48d9eed835ba57b295af7dca9f83723ae85a0f97
saptarshi <sapbum1234@gmail.com>
2024-09-04 16:35:34 +0530

    Request mapping to entire controller

--- src/main/java/com/introduction/ProductServiceAug24/Controllers/ProductController.java
+++ src/main/java/com/introduction/ProductServiceAug24/Controllers/ProductController.java
@@ -15,40 +15,32 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
     @Autowired
     ProductService prod_serve;
 
 
 
-    @GetMapping("/products/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable("id") long prod_id) throws ProductNotFoundExceptions {
-//        if(prod_id<1 || prod_id>20) {
-//            return new ResponseEntity<>(HttpStatusCode.valueOf(400));
-//        }
-//        Product prod= null;
-//        try {
-//            prod = prod_serve.getProduct(prod_id);
-//        } catch (com.introduction.ProductServiceAug24.Exceptions.ProductNotFoundExceptions productNotFoundExceptions) {
-//            throw new RuntimeException(productNotFoundExceptions);
-//        }
         Product prod=prod_serve.getProduct(prod_id);
         return new ResponseEntity<>(prod, HttpStatusCode.valueOf(200));
     }
 
-    @GetMapping("/products/category/jewelery")
+    @GetMapping("/category/jewelery")
     public ResponseEntity<List<Product>>  getJeweleries() {
         List<Product> prod_list=prod_serve.getProductList();
         return new ResponseEntity<>(prod_list, HttpStatusCode.valueOf(200));
     }
 
-    @GetMapping("/products/categories")
+    @GetMapping("/categories")
     public ResponseEntity<Categories> getAllCategories() {
         Categories category=prod_serve.getCategories();
         return new ResponseEntity<>(category, HttpStatusCode.valueOf(200));
     }
 
-    @GetMapping("/products")
+    @GetMapping("")
     public ResponseEntity<List<Product>> limitProducts(@RequestParam("limit") int num) throws ProductLimitOutOfBoundsException {
         List<Product> prod_list=prod_serve.getPopularProducts(num);
         return new ResponseEntity<>(prod_list, HttpStatusCode.valueOf(200));
